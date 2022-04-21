0df22cd1db114ac7ba8dadafc380a3d0abd84917
amenezes-os <andre.menezes@outsystems.com>
2022-04-21 17:51:22 +0100

    Add logs, validation and test steps

--- layout_gnn/model/lightning_module.py
+++ layout_gnn/model/lightning_module.py
@@ -35,20 +35,48 @@ class EncoderDecoderWithTripletLoss(LightningModule):
     def forward(self, x):
         return self.encoder(x)
 
-    def training_step(self, batch, batch_idx):
+    def _step(self, batch, batch_idx):
         x, xp, xn = batch["anchor"], batch["pos"], batch["neg"]
         
         z, zp, zn = self(x), self(xp), self(xn)
 
-        loss = self.triplet_loss(z, zp, zn)
+        triplet_loss = self.triplet_loss(z, zp, zn)
         if self.decoder is not None:
-            y = self.decoder(z)
-            loss += self.reconstruction_loss_weight * self.reconstruction_loss(y, batch["image"])
+            reconstruction_loss = self.reconstruction_loss(self.decoder(z), batch["image"])
+            loss = triplet_loss + self.reconstruction_loss_weight * reconstruction_loss
+            return loss, triplet_loss, reconstruction_loss
+        else:
+            return triplet_loss, None, None
 
-        # TODO: Logging
+
+    def training_step(self, batch, batch_idx):
+        loss, triplet_loss, reconstruction_loss = self._step(batch, batch_idx)
+
+        if triplet_loss is not None:
+            self.log("train_triplet_loss", triplet_loss)
+        if reconstruction_loss is not None:
+            self.log("train_reconstruction_loss", reconstruction_loss)
+        self.log("train_loss", loss, prog_bar=True)
+
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, triplet_loss, reconstruction_loss = self._step(batch, batch_idx)
+
+        if triplet_loss is not None:
+            self.log("val_triplet_loss", triplet_loss)
+        if reconstruction_loss is not None:
+            self.log("val_reconstruction_loss", reconstruction_loss)
+        self.log("val_loss", loss, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        loss, triplet_loss, reconstruction_loss = self._step(batch, batch_idx)
+
+        if triplet_loss is not None:
+            self.log("test_triplet_loss", triplet_loss)
+        if reconstruction_loss is not None:
+            self.log("test_reconstruction_loss", reconstruction_loss)
+        self.log("test_loss", loss, prog_bar=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
-
-    # TODO: validation_step and test_step
