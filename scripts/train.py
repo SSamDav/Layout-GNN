6685ad58a756887c17b9acd9f639452abaf69046
amenezes-os <andre.menezes@outsystems.com>
2022-04-22 19:16:24 +0100

    Reduve margin

--- scripts/train.py
+++ scripts/train.py
@@ -35,7 +35,7 @@ READOUT = lambda x, inputs: global_mean_pool(x, batch=inputs.batch)
 CNN_HIDDEN_DIM = 16
 TRIPLET_LOSS_DISTANCE_FUNCTION = lambda x1, x2: 1 - F.cosine_similarity(x1, x2)
 # Loss/optimizer parameters
-TRIPLET_LOSS_MARGIN = 1.
+TRIPLET_LOSS_MARGIN = .5
 RECONSTRUCTION_LOSS_WEIGHT = 1
 LR = 0.001
 
