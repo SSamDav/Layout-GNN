b717fff70cac80227353b389364272e6ee6272e9
SSamDav <ssam_dav@hotmail.com>
2022-11-01 17:04:53 +0000

    Test different parameters

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
-python hyperparameter_search.py -c /media/disk/workspace/Layout-GNN/train_configs/multimodal_vicreg.yml
-# python train.py -c /media/disk/workspace/Layout-GNN/train_configs/multimodal_ntxent.yml
-# sudo shutdown
+# python hyperparameter_search.py -c /media/disk/workspace/Layout-GNN/train_configs/multimodal_vicreg.yml
+python train.py -c /media/disk/workspace/Layout-GNN/train_configs/multimodal_ntxent.yml
+sudo shutdown
